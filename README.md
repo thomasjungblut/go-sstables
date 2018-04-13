92ccd5d814e87f756ed8c43da6d6e557a670d3d5
Thomas Jungblut <thomas.jungblut@gmail.com>
2018-04-13 22:31:10 +0200

    readme and better benchmark

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
-# go-sstables
+## go-sstables
+
+`go-sstables` is a Go library that contains NoSQL database building blocks like a sequential record format (recordio) and a sorted string table (sstables, indexed using a skip list).
+
+It will come with some protobuf convenience bindings and compression support.
+
+## Installation
+
+> go get -d github.com/thomasjungblut/go-sstables
+
+## Using SSTables
 
 coming soon...
 
-## RecordIO
+## Using RecordIO
 
+coming soon...
+
+### Benchmark 
+
+For writes, here's a simple write benchmark on a SSD.
+Basically writing a thousand records of varying sizes, with normal buffered writes and sync writes after each record.
+
+```
+$ make bench
+go test -v -benchmem -bench=. ./benchmark
+BenchmarkWriteRecordSize1k-12                100          10245670 ns/op         101.90 MB/s        2873 B/op         21 allocs/op
+BenchmarkWriteRecordSize10k-12               100          21366487 ns/op         480.19 MB/s        2934 B/op         20 allocs/op
+BenchmarkWriteRecordSize100k-12               20         151194930 ns/op         677.40 MB/s        8092 B/op         20 allocs/op
+BenchmarkWriteRecordSize1m-12                  2        1427030100 ns/op         734.81 MB/s      527056 B/op         20 allocs/op
+BenchmarkWriteSyncRecordSize1k-12              1        2848388700 ns/op           0.37 MB/s        3864 B/op         23 allocs/op
+BenchmarkWriteSyncRecordSize10k-12             1        3073590800 ns/op           3.34 MB/s       13016 B/op         21 allocs/op
+BenchmarkWriteSyncRecordSize100k-12            1        5032531600 ns/op          20.35 MB/s      109272 B/op         21 allocs/op
+BenchmarkWriteSyncRecordSize1m-12              1        11963311000 ns/op         87.65 MB/s     1051416 B/op         22 allocs/op
+PASS
+ok      github.com/thomasjungblut/go-sstables/benchmark 32.571s
+```
--- benchmark/write_test.go
+++ benchmark/write_test.go
@@ -9,10 +9,15 @@ import (
 	"os"
 )
 
-func BenchmarkWriteRecordSize1k(b *testing.B)   { benchmarkWriteRecordSize(1024, b) }
-func BenchmarkWriteRecordSize10k(b *testing.B)  { benchmarkWriteRecordSize(1024*10, b) }
-func BenchmarkWriteRecordSize100k(b *testing.B) { benchmarkWriteRecordSize(1024*100, b) }
-func BenchmarkWriteRecordSize1m(b *testing.B)   { benchmarkWriteRecordSize(1024*1024, b) }
+func BenchmarkWriteRecordSize1k(b *testing.B)   { benchmarkWriteRecordSize(1024, false, b) }
+func BenchmarkWriteRecordSize10k(b *testing.B)  { benchmarkWriteRecordSize(1024*10, false, b) }
+func BenchmarkWriteRecordSize100k(b *testing.B) { benchmarkWriteRecordSize(1024*100, false, b) }
+func BenchmarkWriteRecordSize1m(b *testing.B)   { benchmarkWriteRecordSize(1024*1024, false, b) }
+
+func BenchmarkWriteSyncRecordSize1k(b *testing.B)   { benchmarkWriteRecordSize(1024, true, b) }
+func BenchmarkWriteSyncRecordSize10k(b *testing.B)  { benchmarkWriteRecordSize(1024*10, true, b) }
+func BenchmarkWriteSyncRecordSize100k(b *testing.B) { benchmarkWriteRecordSize(1024*100, true, b) }
+func BenchmarkWriteSyncRecordSize1m(b *testing.B)   { benchmarkWriteRecordSize(1024*1024, true, b) }
 
 func randomRecordOfSize(l int) []byte {
 	bytes := make([]byte, l)
@@ -24,8 +29,8 @@ func randomRecordOfSize(l int) []byte {
 }
 
 //noinspection GoDeferInLoop
-func benchmarkWriteRecordSize(recSize int, b *testing.B) {
-	const numRecords = 1000 // should be ~1G on the 1m record size
+func benchmarkWriteRecordSize(recSize int, sync bool, b *testing.B) {
+	const numRecords = 1000 // should be max ~1G on the 1m record size
 	bytes := randomRecordOfSize(recSize)
 	for n := 0; n < b.N; n++ {
 		tmpFile, err := ioutil.TempFile("", "recordio_Bench")
@@ -37,10 +42,20 @@ func benchmarkWriteRecordSize(recSize int, b *testing.B) {
 		assert.Nil(b, writer.Open())
 
 		for i := 0; i < numRecords; i++ {
-			_, err := writer.Write(bytes)
-			assert.Nil(b, err)
+			if sync {
+				_, err := writer.WriteSync(bytes)
+				assert.Nil(b, err)
+			} else {
+				_, err := writer.Write(bytes)
+				assert.Nil(b, err)
+			}
 		}
 
 		assert.Nil(b, writer.Close())
+
+		// report the size of the written file for throughput metrics
+		stat, err := os.Stat(tmpFile.Name())
+		assert.Nil(b, err)
+		b.SetBytes(stat.Size())
 	}
 }
