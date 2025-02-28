6bf9d7cdc45b2e3e14f8caaa0202f318b4c270f3
Thomas Jungblut <thomas.jungblut@gmail.com>
2025-02-28 19:46:08 +0100

    refactor sstable index into a dedicated interface with a loader (#48)
    This is the first step to find a better and more performant solution for #47.
    
    Key changes:
    * new index interface
    * new loader interface for an index
    * `sstables.SkipInvalidHashesOnLoad` is now removed
    * sstables comparator defaults to `skiplist.BytesComparator` so it doesn't have to be supplied anymore

--- benchmark/sstable_read_test.go
+++ benchmark/sstable_read_test.go
@@ -1,51 +1,103 @@
 package benchmark
 
 import (
-	"encoding/binary"
 	"errors"
-	"github.com/stretchr/testify/assert"
-	"github.com/thomasjungblut/go-sstables/skiplist"
-	"github.com/thomasjungblut/go-sstables/sstables"
 	"os"
 	"testing"
+	"time"
+
+	"crypto/sha1"
+	"encoding/binary"
+	"github.com/stretchr/testify/assert"
+	"github.com/thomasjungblut/go-sstables/memstore"
+	"github.com/thomasjungblut/go-sstables/skiplist"
+	"github.com/thomasjungblut/go-sstables/sstables"
 )
 
-// we're writing a gig worth of data and test how long it takes to read the index + all data
 func BenchmarkSSTableRead(b *testing.B) {
-	tmpDir, err := os.MkdirTemp("", "sstable_BenchRead")
-	assert.Nil(b, err)
-	defer func() { assert.Nil(b, os.RemoveAll(tmpDir)) }()
-
-	cmp := skiplist.BytesComparator{}
-	writer, err := sstables.NewSSTableStreamWriter(sstables.WriteBasePath(tmpDir), sstables.WithKeyComparator(cmp))
-	assert.Nil(b, err)
-	assert.Nil(b, writer.Open())
-
-	bytes := randomRecordOfSize(1024)
-	for i := 0; i < len(bytes)*1024; i++ {
-		k := make([]byte, 4)
-		binary.BigEndian.PutUint32(k, uint32(i))
-		assert.Nil(b, writer.WriteNext(k, bytes))
+	benchmarks := []struct {
+		name         string
+		memstoreSize int
+	}{
+		{"32mb", 1024 * 1024 * 32},
+		{"64mb", 1024 * 1024 * 64},
+		{"128mb", 1024 * 1024 * 128},
+		{"256mb", 1024 * 1024 * 256},
+		{"512mb", 1024 * 1024 * 512},
+		{"1024mb", 1024 * 1024 * 1024},
+		{"2048mb", 1024 * 1024 * 1024 * 2},
+		{"4096mb", 1024 * 1024 * 1024 * 4},
+		{"8192mb", 1024 * 1024 * 1024 * 8},
 	}
 
-	assert.Nil(b, writer.Close())
-	fullScanTable(b, tmpDir, cmp)
+	cmp := skiplist.BytesComparator{}
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			tmpDir, err := os.MkdirTemp("", "sstable_BenchRead_"+bm.name)
+			assert.Nil(b, err)
+			defer func() { assert.Nil(b, os.RemoveAll(tmpDir)) }()
+
+			mStore := memstore.NewMemStore()
+			bytes := randomRecordOfSize(1024)
+
+			i := 0
+			for mStore.EstimatedSizeInBytes() < uint64(bm.memstoreSize) {
+				kx := make([]byte, 4)
+				binary.BigEndian.PutUint32(kx, uint32(i))
+				hash := sha1.New()
+				hash.Write(kx)
+
+				k := hash.Sum([]byte{})
+				assert.Nil(b, mStore.Add(k, bytes))
+				i++
+			}
+
+			assert.Nil(b, mStore.Flush(sstables.WriteBasePath(tmpDir), sstables.WithKeyComparator(cmp)))
+			defer func() {
+				assert.Nil(b, os.RemoveAll(tmpDir))
+			}()
+
+			b.ResetTimer()
+			fullScanTable(b, tmpDir, cmp)
+		})
+	}
 }
 
 func fullScanTable(b *testing.B, tmpDir string, cmp skiplist.Comparator[[]byte]) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		reader, err := sstables.NewSSTableReader(sstables.ReadBasePath(tmpDir), sstables.ReadWithKeyComparator(cmp))
+		loadStart := time.Now()
+		reader, err := sstables.NewSSTableReader(sstables.ReadBasePath(tmpDir), sstables.ReadWithKeyComparator(cmp), sstables.SkipHashCheckOnLoad())
+		loadEnd := time.Now().Sub(loadStart)
+		b.ReportMetric(float64(loadEnd.Milliseconds()), "load_time_ms")
+		b.ReportMetric(float64(loadEnd.Nanoseconds())/float64(reader.MetaData().NumRecords), "load_time_ns/record")
+		b.ReportMetric(float64(reader.MetaData().IndexBytes), "index_bytes")
+		b.ReportMetric(float64(reader.MetaData().IndexBytes)/1024/1024/loadEnd.Seconds(), "load_bandwidth_mb/s")
+
+		defer func() {
+			assert.Nil(b, reader.Close())
+		}()
+
 		assert.Nil(b, err)
+		scanStart := time.Now()
 		scanner, err := reader.Scan()
 		assert.Nil(b, err)
+		i := uint64(0)
 		for {
 			_, _, err := scanner.Next()
 			if errors.Is(err, sstables.Done) {
 				break
 			}
+			i++
 		}
+		if reader.MetaData().NumRecords != i {
+			b.Fail()
+		}
+		scanEnd := time.Now().Sub(scanStart)
 		b.SetBytes(int64(reader.MetaData().TotalBytes))
-		assert.Nil(b, reader.Close())
+		b.ReportMetric(float64(scanEnd.Milliseconds()), "scan_time_ms")
+		b.ReportMetric(float64(scanEnd.Nanoseconds())/float64(i), "scan_time_ns/record")
+		b.ReportMetric(float64(i), "num_records")
+		b.ReportMetric(float64(reader.MetaData().DataBytes)/1024/1024/scanEnd.Seconds(), "scan_bandwidth_mb/s")
 	}
 }
